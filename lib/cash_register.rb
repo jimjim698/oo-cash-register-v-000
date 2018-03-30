ab1e898ed2e7f7f762093df2bea865adb87a3e57
James Lotruglio <jimlotruglio@yahoo.com>
2018-03-30 00:35:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+attr_accessor :total, :discount
 
+def initialize(discount = 0)
+  @total = 0
+  @discount = discount
+  @cart = []
+
+end
+
+def add_item(item, price,quant = 1)
+    @cart<< item
+    price_with_quantity = price * quant
+    @total = @total + price_with_quantity
+end
+
+def apply_discount
+  float_discount = (@discount*0.01*@total).to_i
+  @total = @total - float_discount
+success = "After the discount, the total comes to $#{@total}."
+error = "There is no discount to apply."
+if @discount>0
+   success
+else
+   error
+end
+end
+
+
+
+end
